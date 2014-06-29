58f183252af77ee4e1f52626a902636712eb93a7
Amid Ale <amidale@gmail.com>
2014-06-29 20:33:27 -0300

    5.12 Using partials to clean up duplication in views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-  end  
+  end
 
   def show
     @article = Article.find(params[:id])
@@ -11,26 +11,30 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
+  def edit
+   @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render 'new'
-    end  
+    end
 end
 
 def update
   @article = Article.find(params[:id])
- 
+
   if @article.update(article_params)
     redirect_to @article
   else
     render 'edit'
   end
 end
- 
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
