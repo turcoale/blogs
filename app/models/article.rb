deaed8e43f53210efe6b999ec1b2853ca5ca96f2
Amid Ale <amidale@gmail.com>
2014-06-29 21:05:46 -0300

    6.2 Associating Models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
+  has_many :comments
+
   validates :title, presence: true,
                     length: { minimum: 5 }
-  has_many :comments
 
   def ultimo
     comments.last
